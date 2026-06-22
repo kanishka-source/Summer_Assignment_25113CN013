8b729ab64b6a976cb8e20b1a4dc902b88d9c32a6
kanishka-source <kanishkavikal@gmail.com>
2026-06-22 18:01:48 +0530

    DAY21

---
+++ DAY21/Question83.py
@@ -0,0 +1,14 @@
+s = input("Enter a string: ")
+
+vowels = 0
+consonants = 0
+
+for ch in s.lower():
+    if ch.isalpha():
+        if ch in "aeiou":
+            vowels += 1
+        else:
+            consonants += 1
+
+print("Vowels =", vowels)
+print("Consonants =", consonants)
