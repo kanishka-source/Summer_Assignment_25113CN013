61270b0c62b96d198efdf8c22819d4914554468d
kanishka-source <kanishkavikal@gmail.com>
2026-06-23 23:14:30 +0530

    DAY23

---
+++ DAY23/Question92.py
@@ -0,0 +1,12 @@
+s = input("Enter a string: ")
+
+max_char = s[0]
+max_count = s.count(max_char)
+
+for ch in s:
+    if s.count(ch) > max_count:
+        max_count = s.count(ch)
+        max_char = ch
+
+print("Maximum occurring character:", max_char)
+print("Frequency:", max_count)
